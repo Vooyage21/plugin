076aaba5d1b167c56162e8fa47ee84b6f9804c66
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-20 01:10:46 +0700

    Update advice.py

--- plugins/advice.py
+++ plugins/advice.py
@@ -20,14 +20,14 @@ async def advice(_, message):
         c = a["date"]
         url = a["imageUrl"]
         b = a["explanation"]
-        caption = f"Tᴏᴅᴀʏ's [{c}] ᴀsᴛʀᴏɴᴏᴍɪᴄᴀʟ ᴇᴠᴇɴᴛ:\n\n{b}"
+        caption = f"Today's [{c}] astronomical event:\n\n{b}"
         await message.reply_photo(url, caption=caption)
     else:
-        await message.reply_photo("ᴛʀʏ ᴀғᴛᴇʀ sᴏᴍᴇ ᴛɪᴍᴇ")
+        await message.reply_photo("try after some time")
         await app.send_message(LOG_GROUP_ID, "/astronomical not working")
 
 
-__MODULE__ = "Aᴅᴠɪᴄᴇ"
-__HELP__ = """
-/advice - Gᴇᴛ ʀᴀɴᴅᴏᴍ ᴀᴅᴠɪᴄᴇ
-/astronomical - ᴛᴏ ɢᴇᴛ ᴛᴏᴅᴀʏ's ᴀsᴛʀᴏɴᴏᴍɪᴄᴀʟ  ғᴀᴄᴛ"""
+__MODULE__ = "Device"
+__HELP__ = """<blockquote><b>
+/advice - Get random advice
+/astronomical - to get today's astronomical  fact</b></blockquote>"""
