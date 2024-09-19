b00f2c736077b08b9e32e029588b0bb4052f5a74
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-09-20 01:34:12 +0700

    update plugins/pretender.py

--- plugins/pretender.py
+++ plugins/pretender.py
@@ -3,10 +3,10 @@ from typing import Dict, Union
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from pyrogram import filters
 from pyrogram.types import Message
-from pyrogram.enums import ChatMembersFilter
 
 from config import MONGO_DB_URI
 from YukkiMusic import app
+from YukkiMusic.utils.filter import admin_filter
 
 mongo = MongoCli(MONGO_DB_URI).Rankings
 
@@ -68,90 +68,76 @@ async def chk_usr(_, message: Message):
             message.from_user.last_name,
         )
         return
-
     usernamebefore = user_data.get("username", "")
     first_name = user_data.get("first_name", "")
     lastname_before = user_data.get("last_name", "")
-
-    msg = f"[{message.from_user.id}](tg://user?id={message.from_user.id})\n"
-
-    changes = []
-
+    msg = ""
     if (
-        first_name != message.from_user.first_name
-        and lastname_before != message.from_user.last_name
+        usernamebefore != message.from_user.username
+        or first_name != message.from_user.first_name
+        or lastname_before != message.from_user.last_name
     ):
-        changes.append(
-            f"ᴄʜᴀɴɢᴇᴅ ʜᴇʀ ɴᴀᴍᴇ ғʀᴏᴍ {first_name} {lastname_before} ᴛᴏ {message.from_user.first_name} {message.from_user.last_name}\n"
-        )
-    elif first_name != message.from_user.first_name:
-        changes.append(
-            f"ᴄʜᴀɴɢᴇᴅ ʜᴇʀ ғɪʀsᴛ ɴᴀᴍᴇ ғʀᴏᴍ {first_name} ᴛᴏ {message.from_user.first_name}\n"
-        )
-    elif lastname_before != message.from_user.last_name:
-        changes.append(
-            f"ᴄʜᴀɴɢᴇᴅ ʜᴇʀ ʟᴀsᴛ ɴᴀᴍᴇ ғʀᴏᴍ {lastname_before} ᴛᴏ {message.from_user.last_name}\n"
+        if (
+            first_name != message.from_user.first_name
+            and lastname_before != message.from_user.last_name
+        ):
+            msg += f"""user **{message.from_user.mention}** changed her name from {first_name} {lastname_before} to {message.from_user.first_name} {message.from_user.last_name}\n"""
+        elif first_name != message.from_user.first_name:
+            msg += f"""user **{message.from_user.mention}** changed her first name from {first_name} to {message.from_user.first_name}\n"""
+        elif lastname_before != message.from_user.last_name:
+            msg += f"""user **{message.from_user.mention}** changed her last name from {lastname_before} to {message.from_user.last_name}\n"""
+
+        if usernamebefore != message.from_user.username:
+            msg += f"""user **{message.from_user.mention}** changed her username from @{usernamebefore} to @{message.from_user.username}\n"""
+
+        await add_userdata(
+            chat_id,
+            user_id,
+            message.from_user.username,
+            message.from_user.first_name,
+            message.from_user.last_name,
         )
 
-    if usernamebefore != message.from_user.username:
-        changes.append(
-            f"ᴄʜᴀɴɢᴇᴅ ʜᴇʀ ᴜsᴇʀɴᴀᴍᴇ ғʀᴏᴍ @{usernamebefore} ᴛᴏ @{message.from_user.username}\n"
-        )
-
-    if changes:
-        msg += "".join(changes)
+    if msg != "":
         await message.reply_text(msg)
-
-    await add_userdata(
-        chat_id,
-        user_id,
-        message.from_user.username,
-        message.from_user.first_name,
-        message.from_user.last_name,
-    )
 
 
 @app.on_message(
-    filters.group & filters.command("pretender") & ~filters.bot & ~filters.via_bot
+    filters.group
+    & filters.command("pretender")
+    & ~filters.bot
+    & ~filters.via_bot
+    & admin_filter
 )
 async def set_mataa(_, message: Message):
-    admin_ids = [
-        admin.user.id
-        async for admin in app.get_chat_members(
-            message.chat.id, filter=ChatMembersFilter.ADMINISTRATORS
-        )
-    ]
-    if message.from_user.id not in admin_ids:
-        return
     if len(message.command) == 1:
-        return await message.reply("**ᴅᴇᴛᴇᴄᴛᴇᴅ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴜsᴀɢᴇ:\n/pretender on|off**")
+        return await message.reply("**detected pretender usage:\n/pretender on|off**")
     chat_id = message.chat.id
     if message.command[1] == "on":
         cekset = await check_pretender(chat_id)
         if cekset:
             await message.reply(
-                f"ᴘʀᴇᴛᴇɴᴅᴇʀ ɪs ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ ғᴏʀ **{message.chat.title}**"
+                f"pretender is already enabled for **{message.chat.title}**"
             )
         else:
             await impo_on(chat_id)
             await message.reply(
-                f"sᴜᴄᴇssғᴜʟʟʏ ᴇɴᴀʙʟᴇᴅ ᴘʀᴇᴛᴇɴᴅᴇʀ ғᴏʀ **{message.chat.title}**"
+                f"sucessfully enabled pretender for **{message.chat.title}**"
             )
     elif message.command[1] == "off":
         cekset = await check_pretender(chat_id)
         if not cekset:
             await message.reply(
-                f"ᴘʀᴇᴛᴇɴᴅᴇʀ ɪs ᴀʟʀᴇᴀᴅʏ ᴅɪsᴀʙʟᴇᴅ ғᴏʀ **{message.chat.title}**"
+                f"pretender is already disabled for **{message.chat.title}**"
             )
         else:
             await impo_off(chat_id)
             await message.reply(
-                f"sᴜᴄᴇssғᴜʟʟʏ ᴅɪsᴀʙʟᴇᴅ ᴘʀᴇᴛᴇɴᴅᴇʀ ғᴏʀ **{message.chat.title}"
+                f"sucessfully disabled pretender for **{message.chat.title}"
             )
     else:
-        await message.reply("**ᴅᴇᴛᴇᴄᴛᴇᴅ ᴘʀᴇᴛᴇɴᴅᴇʀ ᴜsᴀɢᴇ:\n/pretender on|off**")
+        await message.reply("**detected pretender usage:\n/pretender on|off**")
 
-
-__MODULE__ = "Pʀᴇᴛᴇɴᴅᴇʀ"
-__HELP__ = """
-/pretender - [Oɴ / ᴏғғ]  - ᴛᴏ ᴛᴜʀɴ ᴏɴ ᴏʀ ᴏғғ ᴘʀᴇᴛᴇɴᴅᴇʀ ғᴏʀ ʏᴏᴜ ᴄʜᴀᴛ ɪғ ᴀɴʏ ᴜsᴇʀ ᴄʜᴀɴɢᴇ ʜᴇʀ ᴜsᴇʀɴᴀᴍᴇ, ɴᴀᴍᴇ , ʙɪᴏ ʙᴏᴛ ᴡɪʟʟ sᴇɴᴅ ᴍᴇssᴀɢᴇ ɪɴ ʏᴏᴜʀ ᴄʜᴀᴛ"""
+__MODULE__ = "Tender"
+__HELP__ = """<blockquote><b>
+/pretender - [On / off]  - to turn on or off pretender for you chat if any user change her username, name , bio bot will send message in your chat</b></blockquote>"""
